709429a145fc1442e3c58ec417172174ea3e2b83
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-08-24 00:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
-
+let input = document.getElementById("newTodoInput");
+let button = document.getElementById("addTodoBtn");
+let toDoList = document.getElementById("todoList");
+button.addEventListner("click", () => {
+	if(input.textContent != ""){
+		toDoList.textContent = input.value;
+	}
+});
